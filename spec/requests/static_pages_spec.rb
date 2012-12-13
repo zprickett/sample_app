bb106769b6b2743d9f645958d8f6432478583ec8
zprickett <zprickett@gmail.com>
2012-12-13 14:17:03 +0800

    New title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,11 +11,18 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have a title" do
+    it "should have base title" do
       
       visit '/static_pages/home'
 
       page.should have_selector('title', :text => 'Ruby on Rails | Sample App')
+    end
+
+    it "should not have a custom page title" do
+      
+      visit '/static_pages/home'
+
+      page.should_not have_selector('title', :text => '| Home' )
     end
   end
 
